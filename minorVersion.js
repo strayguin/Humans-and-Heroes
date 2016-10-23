6544588efc422ba62c4c722efea4442847acb53f
Ryan Worcester <rworcest@g.emporia.edu>
2016-10-22 19:40:14 -0500

    3.2 Small changes: affects others, attack, area, enhanced trait

--- minorVersion.js
+++ minorVersion.js
@@ -1,5 +1,5 @@
 /**This uses var rather than const because the value is only used once and (unlike point counter)
 the front page needs to be easy for any browser to load.*/
-var latestMinorVersion = 1;
+var latestMinorVersion = 2;
 
 //major version isn't here because it is intentionally difficult to increment
