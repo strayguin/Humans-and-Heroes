ea825b6d9a1018514842440cd65a198bfc75841b
Ryan Worcester <rworcest@g.emporia.edu>
2016-10-30 14:03:31 -0500

    3.3 Roll init 1/combat. round contains turns

--- minorVersion.js
+++ minorVersion.js
@@ -1,5 +1,5 @@
 /**This uses var rather than const because the value is only used once and (unlike point counter)
 the front page needs to be easy for any browser to load.*/
-var latestMinorVersion = 2;
+var latestMinorVersion = 3;
 
 //major version isn't here because it is intentionally difficult to increment
